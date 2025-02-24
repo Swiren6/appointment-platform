206778bad3de76423db99b71f510f36bfb640612
abdelkhaleksirinette6 <abdelkhaleksirinette6@gmail.com>
2025-02-24 13:27:40 +0100

    users CRUD

---
+++ routes/UserR.js
@@ -0,0 +1,48 @@
+  const express = require("express");
+  const User = require("../models/User");
+  const router = express.Router();
+
+  // 1)Récupérer tous les utilisateurs
+  router.get("/users/all", async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+  });
+
+
+  //2)Récupérer un utilisateur par ID
+  router.get("/user/:id", async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "404:User Not Found" });
+    res.json(user);
+  });
+
+
+  //3)Créer un nouvel utilisateur
+  router.post("/user/create", async (req, res) => {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).json(newUser);
+  });
+
+
+  // 4)Mettre à jour un utilisateur
+  router.put("/user/update/:id", async (req, res) => {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    res.status(201).json(updatedUser);
+  });
+
+
+  //5)Supprimer un utilisateur
+  router.delete("/user/delete/:id", async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(201).json({ message: "User Deleted " });
+  });
+
+
+
+  module.exports = router;
+
+
+
--- server.js
+++ server.js
@@ -3,18 +3,26 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 
+const userRoutes = require("./routes/UserR");
+
+
 dotenv.config();
-
 const app = express();
+app.use(express.json());//beche ya9ra les requests en format json
 
-app.use(express.json());
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
 
+  .then(() => console.log("✅ Connecté à MongoDB"))
+  .catch(err => console.error(err));
+  app.use("/users", userRoutes);//activer les routes users 
 
+  app.get("/", (req, res) => {
+    res.send("Welcome ");
+  });
 
-
-
-// Démarrer le serveur
+// machi el serveur
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT}`);
+  console.log(`✅Serveur démarré sur le port ${PORT}`);
 });
